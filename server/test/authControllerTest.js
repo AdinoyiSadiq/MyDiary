2baa489a558857e969e3f39304a7334bc8db765e
AdinoyiSadiq <sadiqadinoyi@gmail.com>
2018-07-29 07:49:45 +0100

    chore(travis): add first signin test
    - Ensure that test for signin passes in travis
    
    [Finishes #159381385]

--- server/test/authControllerTest.js
+++ server/test/authControllerTest.js
@@ -143,19 +143,19 @@ describe('Authentication controller', () => {
         });
     });
 
-  //   it('POST to /api/v1/auth/signin should create a authenticate a user using username and password', done => {
-  //     request(app)
-  //       .post('/api/v1/auth/signin')
-  //       .send({
-  //         "email": "adinoyi@gmail.com",
-  //         "password": "myPassword"
-  //       })
-  //       .end((err, res) => {
-  //         expect(res.status).to.equal(200);
-  //         expect(res.body.token).to.be.a('string');
-  //         done();
-  //       });
-  //   });
+    it('POST to /api/v1/auth/signin should create a authenticate a user using username and password', done => {
+      request(app)
+        .post('/api/v1/auth/signin')
+        .send({
+          "email": "sadiqadinoyi@gmail.com",
+          "password": "myPassword"
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.token).to.be.a('string');
+          done();
+        });
+    });
 
   //   it('Should return an error message when the user tries to sign in with no data', done => {
   //     request(app)

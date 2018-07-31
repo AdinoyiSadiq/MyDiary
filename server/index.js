143303f6f724548bce9da9d1ff410191530e9d9f
AdinoyiSadiq <sadiqadinoyi@gmail.com>
2018-07-31 15:02:36 +0100

    feat(signup): add signup functionality for the user through UI
    - Ensure that when a user fills the signup fields and submits then the user is authenticated and signed up
    
    [Delivers #159395024]

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import http from 'http';
 import bodyParser from 'body-parser';
 import router from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json({ type: 'application/json' }));
 app.use('/api/v1/', router);
 

34432a3d04d8754407efbf4455aa52f705cea8b4
AdinoyiSadiq <sadiqadinoyi@gmail.com>
2018-08-01 23:58:14 +0100

    feat(createEntry): add create entry functionality to the UI
    - Enable the user to create entries using the UI
    
    [Delivers #159425412]

---
+++ client/scripts/addEntry.js
@@ -0,0 +1,46 @@
+window.onload = () => {
+  const url = 'http://localhost:3090/api/v1/entries';
+  const token = window.localStorage.getItem('token');
+  const createEntryError = document.getElementById('createEntryError');
+  const title = document.getElementById('title');
+  const content = document.getElementById('content');
+
+  function createEntry(event) {
+    event.preventDefault();
+
+    const values = {
+      title: title.value,
+      content: content.value,
+    };
+
+    window.fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: token,
+      },
+      body: JSON.stringify({
+        title: values.title,
+        content: values.content,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 500) {
+          createEntryError.innerHTML = 'An error occurred while processing this request';
+          return;
+        }
+        return (response.json());
+      })
+      .then((data) => {
+        const { message } = data;
+        if (message.startsWith('Please fill')) {
+          createEntryError.innerHTML = message;
+        } else if (message === 'Diary Entry Created Successfully') {
+          window.location.replace(`./entry.html#${data.entry.id}`);
+        }
+      });
+  }
+
+  document.getElementById('save').addEventListener('click', createEntry, false);
+  // document.getElementById('cancel').addEventListener('click', signup, false);
+};
